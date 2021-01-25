9c2e4c38e480587efd89021aa00b62de483df71a
Abastro <abab9579@naver.com>
2021-01-25 12:20:44 +0900

    Moved examples to test

--- src/Data/Column.hs
+++ src/Data/Column.hs
@@ -3,6 +3,7 @@ module Data.Column (
   , module Data.Impl.Column
   , module Data.Impl.Classes
   , module Data.Impl.Singles
+  , module Data.Impl.Aggregates
 ) where
 
 import Data.RefTuple (
@@ -17,24 +18,3 @@ import Data.Impl.Column (
 import Data.Impl.Classes ( With(..), With1(..) )
 import Data.Impl.Singles
 import Data.Impl.Aggregates
-
--- TODO Why are examples here..
-example1 :: (WithCond f, Numeric f) => Single f -> Column f p
-example1 ident = buildCol $ do
-  c <- refer $ known "Class"
-  wherein $ c:."priority" :>=: wrapI 0 `Cand` c:."course" :=: ident
-  t <- refer $ known "Time"
-  wherein $ c:."sem" :=: t:."sem"
-  order [Asc "begin", Dsc "end"] . lift $ select ["course", "begin", "end"] c
-
-example2 :: (WithCond f, Aggregates p) => Column f p
-example2 = buildCol $ do
-  c <- refer $ known "Class"
-  t <- refer $ known "Time"
-  wherein $ c:."sem" :=: t:."sem"
-  order [Dsc "numClasses"] . partition (select ["ident"] c) [
-    "ident" `indexAs` "class"
-    , ccount `as` "numClasses"
-    , cmin "begin" `as` "first"
-    , cmax "begin" `as` "last"
-    ] $ lift c
